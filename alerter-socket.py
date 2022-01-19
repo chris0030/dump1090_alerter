37d2f9b6c7dabdcd029b166925a757ffdda8d100
Chris Weaver <chrisweaver@pop-os.localdomain>
2022-01-19 08:28:06 +0000

    Re-add terminal clear

--- alerter-socket.py
+++ alerter-socket.py
@@ -112,6 +112,7 @@ if __name__ == "__main__":
                     aircrafts.append(ac)
                     updated = True
                 if updated:
+                    os.system('cls' if os.name == 'nt' else 'clear')
                     table_data = [TABLE_HEADERS]
                     for ac in aircrafts:
                         table_data.append(ac.return_table_row())
