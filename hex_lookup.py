95c5e8f3a4f412b9b4380525e76da25fe7ad89ec
Chris Weaver <chrisweaver@pop-os.localdomain>
2022-01-19 08:10:02 +0000

    Add more hex models

--- hex_lookup.py
+++ hex_lookup.py
@@ -1,10 +1,14 @@
 MODEL = {
     "A319": "Airbus A-319",
     "B788": "BOEING 787-8 Dreamliner",
+    "A320": "Airbus A320-214",
+    "A333": "Airbus A330-343X",
 }
 
 HEX_LOOKUP = {
     "400EFC": MODEL['A319'],
     "4074E2": MODEL['B788'],
     "48506B": MODEL['B788'],
+    "4CA63A": MODEL['A320'],
+    "40641d": MODEL['A333'],
 }
